dcab42ae9e9764751ac18fdf94c4d23ef5f0ca90
JIMMY82123 <czc474028460@gmail.com>
2025-08-07 15:01:02 +0800

    🔧 修复折扣表单路由问题 - 添加BaziFormDiscount组件导入 - 添加/form-bazi-discount路由配置 - 解决折扣表单页面只显示页脚和导航栏的问题

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const FAQ = lazy(() => import('./pages/FAQ'))
 const Testimonials = lazy(() => import('./pages/Testimonials'))
 const FreeBaziReport = lazy(() => import('./pages/FreeBaziReport'))
 const BaziForm = lazy(() => import('./pages/BaziForm'))
+const BaziFormDiscount = lazy(() => import('./pages/BaziFormDiscount'))
 const LoveForm = lazy(() => import('./pages/LoveForm'))
 const TalismanForm = lazy(() => import('./pages/TalismanForm'))
 const WealthSign = lazy(() => import('./pages/WealthSign'))
@@ -38,6 +39,7 @@ function App() {
               <Route path="/testimonials" element={<Testimonials />} />
               <Route path="/free-bazi-report" element={<FreeBaziReport />} />
               <Route path="/bazi-form" element={<BaziForm />} />
+              <Route path="/form-bazi-discount" element={<BaziFormDiscount />} />
               <Route path="/love-form" element={<LoveForm />} />
               <Route path="/talisman-form" element={<TalismanForm />} />
               <Route path="/wealth-sign" element={<WealthSign />} />
